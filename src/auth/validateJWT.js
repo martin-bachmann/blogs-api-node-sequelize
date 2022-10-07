b3276074749113314edb7e87f5902a632f29e7b6
Martin Bachmann <martinpezbach@gmail.com>
2022-10-06 21:12:39 -0300

    fix: fix token authentication

--- src/auth/validateJWT.js
+++ src/auth/validateJWT.js
@@ -8,7 +8,7 @@ const secret = process.env.JWT_SECRET || 'seusecretdetoken';
 const validateJWT = async (req, res, next) => {
   const token = req.header('Authorization');
   if (!token) {
-    return res.status(401).json({ error: 'Token not found' });
+    return res.status(401).json({ message: 'Token not found' });
   }
 
   try {
